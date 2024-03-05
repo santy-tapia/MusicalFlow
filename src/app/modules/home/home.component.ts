2630eb594a2cf91896e473fe784eb7fc6cba5a86
Santy <102901822+santy-tapia@users.noreply.github.com>
2024-03-05 12:44:58 +0100

    hay que hacer el carousel de cartas

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -3,55 +3,29 @@ import { SpotifyService } from '../../servicio/spotify.service';
 import { Novedades, Resultado } from '../../interfaces/new-release.interface';
 import { RouterLink } from '@angular/router';
 import { ImageURLPipe } from '../../pipes/image-url.pipe';
-import {CarouselModule} from 'primeng/carousel';
+import { CarouselModule } from 'primeng/carousel';
 import { ThemeService } from '../../servicio/theme.service';
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [RouterLink, ImageURLPipe, CarouselModule],
   templateUrl: './home.component.html',
-  styles: ``
+  styles: ``,
 })
-export class HomeComponent implements OnInit{
+export class HomeComponent {
+   public albums?:Novedades;
 
-  responsiveOptions: any[] | undefined;
-  public albums?:any[];
-
-  constructor(private servicio:SpotifyService, public themeService: ThemeService){
-    console.log(servicio.getNovedades());
-      this.servicio.getNovedades()
-      .then( (datos:any) => {
-        this.albums = datos;
-        console.log(this.albums)
-      })
+    constructor(private servicio:SpotifyService, public themeService: ThemeService){
+      console.log(servicio.getNovedades());
+        this.servicio.getNovedades()
+        .then( (datos:Novedades) => {
+          this.albums = datos;
+          console.log(this.albums)
+        })
 
 
-  }
+    }
   get theme() {
     return this.themeService.darkMode ? 'dark' : 'light';
   }
-  ngOnInit() {
-    
-
-   this.responsiveOptions = [
-        {
-            breakpoint: '1199px',
-            numVisible: 1,
-            numScroll: 1
-        },
-        {
-            breakpoint: '991px',
-            numVisible: 2,
-            numScroll: 1
-        },
-        {
-            breakpoint: '767px',
-            numVisible: 1,
-            numScroll: 1
-        }
-    ];
-}
-
-
-
 }
